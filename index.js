fe565f9bc116abf222ed5508e542a15c3bfc31ce
Joseph <josefophe@gmail.com>
2021-02-07 21:57:26 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const {TonClient} = require("@tonclient/core");
+const {libNode} = require("@tonclient/lib-node");
+
+
+(async () => {
+    try {
+        // Link the platform-dependable TON-SDK binary with the target Application in Typescript
+        // This is a Node.js project, so we link the application with `libNode` binary 
+        // from `@tonclient/lib-node` package
+        // If you want to use this code on other platforms, such as Web or React-Native,
+        // use  `@tonclient/lib-web` and `@tonclient/lib-react-native` packages accordingly
+        // (see README in  https://github.com/tonlabs/ton-client-js )
+        TonClient.useBinaryLibrary(libNode);
+        const client = new TonClient({
+            network: { 
+                // Local node URL here
+                server_address: 'http://localhost'
+            }
+        });
+        console.log("Hello localhost TON!");
+        await main(client);
+        process.exit(0);
+    } catch (error) {
+        console.error(error);
+    }
+})();
